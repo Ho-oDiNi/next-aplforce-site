0c0fe4936c6104aac457177edfb6c66230629954
Ho_oDiNi <Evgen5060@gmail.com>
2025-08-18 04:38:35 +0000

    style(prettier): fавтоисправление элементов

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,25 +1,25 @@
-import TitleSection from "@shared/TitleSection";
-import ServiceSection from "./_components/ServiceSection";
-import AdvantageSection from "./_components/AdvantageSection";
-import ExampleSection from "./_components/ExampleSection";
 import RewiewSection from "./_components/RewiewSection";
-import AnswerSection from "./_components/AnswerSection";
+import ServiceSection from "./_components/ServiceSection";
+import TitleSection from "@shared/TitleSection";
+import AdvantageSection from "@shared/AdvantageSection";
+import ExampleSection from "@shared/ExampleSection";
+import AnswerSection from "@shared/AnswerSection";
 import ConsultSection from "@shared/ConsultSection";
 
 export default function Home() {
-  return (
-    <>
-        <TitleSection
-          title="Ремонт многоквартирных домов в Новосибирске" 
-          description="Профессиональный ремонт кровель, герметизация швов, высотные работы методом промышленного альпинизма. Работаем с ТСЖ и УК. Осмотр швов в день обращения. Гарантия до 5 лет." 
-          hasMiniAdvantage={true} 
-        />
-        <ServiceSection />
-        <AdvantageSection />
-        <ExampleSection />
-        <RewiewSection />
-        <AnswerSection />
-        <ConsultSection isWhite={true} />
-    </>
-  );
+    return (
+        <>
+            <TitleSection
+                title="Ремонт многоквартирных домов в Новосибирске"
+                description="Профессиональный ремонт кровель, герметизация швов, высотные работы методом промышленного альпинизма. Работаем с ТСЖ и УК. Осмотр швов в день обращения. Гарантия до 5 лет."
+                hasMiniAdvantage={true}
+            />
+            <ServiceSection />
+            <AdvantageSection />
+            <ExampleSection />
+            <RewiewSection />
+            <AnswerSection />
+            <ConsultSection isWhite={true} />
+        </>
+    );
 }
--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -2,15 +2,15 @@ import TitleSection from "@shared/TitleSection";
 import ContactSection from "./_components/ContactSection";
 import ConsultSection from "@shared/ConsultSection";
 
-export default function Contact() {
-  return (
-    <>
-    <TitleSection
-        title="Ремонт межпанельных швов в Новосибирске"
-        description="Профессиональная герметизация швов между панелями с использованием современных герметиков и технологий для полного устранения протечек."
-    />
-    <ContactSection />
-    <ConsultSection />
-    </>
-  ); 
+export default function Contacts() {
+    return (
+        <>
+            <TitleSection
+                title="Ремонт межпанельных швов в Новосибирске"
+                description="Профессиональная герметизация швов между панелями с использованием современных герметиков и технологий для полного устранения протечек."
+            />
+            <ContactSection />
+            <ConsultSection />
+        </>
+    );
 }
