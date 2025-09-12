c1e667c23b80f1c2fdd3f7e7ace89e468b79bcf7
Ho-oDiNi <Evgen5060@gmail.com>
2025-09-12 18:13:10 +0700

    fix(css): исправил подзаголовок в услугах

--- components/shared/ServiceMain/ServiceHeader.tsx
+++ components/shared/ServiceMain/ServiceHeader.tsx
@@ -12,7 +12,7 @@ export default function ServiceHeader({
     return (
         <hgroup className="text-start">
             <h2 className="font-bold">{title}</h2>
-            <p className="mt-2 text-xl font-bold text-blue-900">
+            <p className="xs:text-xl mt-2 mb-6 text-lg font-bold text-blue-900 md:text-2xl lg:text-3xl">
                 {description}
             </p>
         </hgroup>
